54ae8549c8d9d83d1b96077e8293ae894c981690
OmarHuang <62143192+OmarHuang@users.noreply.github.com>
2022-01-11 23:28:47 +0800

    Fix indent and add comment

--- ping_test.sh
+++ ping_test.sh
@@ -5,11 +5,13 @@ target="192.168.1.90"
 
 for i in {1..5}; do
     ping -c 3 $target
+    # Check if target is okay and break the loop
     if [ "$?" -eq 0 ]; then
         echo "$(date) $target is okay." >> /var/log/ping_test.log
         break
+    # Check if target is unreachable and reach maximum attempt
     elif [ "$i" -eq 5 ]; then
-	    echo "$(date) $target is unreachable." >> /var/log/ping_test.log
+    	echo "$(date) $target is unreachable." >> /var/log/ping_test.log
         sudo shutdown -h now
         break
     else
